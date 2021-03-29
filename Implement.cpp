9206524b96fa560e72220d9292528c2cb744291a
Yuchen Zhang <47722198+orangeoctopus96@users.noreply.github.com>
2021-03-29 18:09:49 +0800

    Add files via upload

--- Implement.cpp
+++ Implement.cpp
@@ -6,12 +6,10 @@
 #include<cstdlib>
 using namespace std;
 
-// INFINIT is used to represent the edge between x and y doesn't exist 
-const int INFINIT=9999;
+// DEADBEEF is used to represent the edge between two nodes doesn't exist 
+const int DEADBEEF=9999;
 
-//==============================================================================
 // Node definitions
-//==============================================================================
 typedef struct Nodeinfo Node;
 struct Nodeinfo
 {
@@ -20,29 +18,26 @@ struct Nodeinfo
   list<Node> edges;	
 };
 
-//==============================================================================
 // Graph Class
-// Implement Graph via adjacent list
-//==============================================================================
 class Graph
 {
 public:
   Graph();
-  Graph(int Numbers, int Value);
+  Graph(int Numbers, double density, int min, int max);
   int get_edge_value(int x, int y);
   void set_edge_value(int x, int y, int v);
   bool adjacent(int x, int y);
   list<int> neighbors(int x);
   int V();
   int E();
-  list<int> Vertices();
+  list<int> vertices();
   void display();
 
 private:
   int VerticesNum;			// Number of nodes of the Graph
   int EdgesNum;			// Number of edges of the Graph
   list<Node> AdjMatrix;		// Adjacency list representing the Graph
-}
+};
 
 // Create an empty graph
 Graph::Graph()
@@ -66,7 +61,7 @@ Graph::Graph(int Numbers, double density, int min, int max)
     NewNode.number = i;
     NewNode.weight = 0;
     NewNode.edges.clear();
-    AdjMatrix.push_back(newNode);
+    AdjMatrix.push_back(NewNode);
   }
   srand(time(0));
   for (int i=0; i<VerticesNum; ++i)
@@ -81,7 +76,6 @@ Graph::Graph(int Numbers, double density, int min, int max)
       }
     }
   }
-
 }
 
 
@@ -98,15 +92,14 @@ int Graph::get_edge_value(int x, int y)
         return (*j).weight; 
       }
   }
-  return INFINIT;
+  return DEADBEEF;
 }
 
 // sets the value associated to the edge (x,y) to value
 void Graph::set_edge_value(int x, int y, int value)
 {
   bool found;
-  // Add 'y' in the list of 'x' neighbors (if doesn't exist)
-  // Set edge weight to value
+  // for the matrix [x,y]
   for(list<Node>::iterator i=AdjMatrix.begin(); i != AdjMatrix.end(); ++i)
   {
     if ((*i).number==x)
@@ -114,7 +107,7 @@ void Graph::set_edge_value(int x, int y, int value)
       found = false;
       for(list<Node>::iterator j=(*i).edges.begin(); j != (*i).edges.end(); ++j)
       {
-        if ((*j).number==nodeNumbers[y])
+        if ((*j).number==y)
         {  
           (*j).weight=value;
           found = true;
@@ -130,6 +123,33 @@ void Graph::set_edge_value(int x, int y, int value)
       }
     }
   }
+
+  // for the matrix [y,x]
+  for(list<Node>::iterator i=AdjMatrix.begin(); i != AdjMatrix.end(); ++i)
+  {
+    if ((*i).number==y)
+    {
+      found = false;
+      for(list<Node>::iterator j=(*i).edges.begin(); j != (*i).edges.end(); ++j)
+      {
+        if ((*j).number==x)
+        {  
+          (*j).weight=value;
+          found = true;
+        }
+      }
+      if (!found)
+      {
+        Node NewNode;
+        NewNode.number = x;
+        NewNode.weight = value;
+        NewNode.edges.clear();
+        (*i).edges.push_back(NewNode);
+        ++EdgesNum;
+      }
+    }
+  }
+}
 
 // tests whether there is an edge from node x to node y
 bool Graph::adjacent(int x, int y)
@@ -193,37 +213,42 @@ list<int> Graph::vertices()
 // Display the Graph
 void Graph::display()
 {
-  cout << "  ";
+  cout << "   ";
   for(list<Node>::iterator i=AdjMatrix.begin(); i != AdjMatrix.end(); ++i)
-    cout << " " << (*i).number;
+    if ((*i).number < 10) 
+      cout << " " << "0" << (*i).number;
+    else
+      cout << " " << (*i).number;
   cout << endl;
   for(list<Node>::iterator i=AdjMatrix.begin(); i != AdjMatrix.end(); ++i)
   {
-    cout << " " << (*i).number;
+    if ((*i).number < 10) 
+      cout << " " << "0" << (*i).number;
+    else
+      cout << " " << (*i).number;
     int position=0; // display point now
     for(list<Node>::iterator j=(*i).edges.begin(); j != (*i).edges.end(); ++j)
     {
       int shift=(*j).number - position; // width between display point now and display node now
       for(int h=0; h<shift; ++h)
       {
-        cout << " -";
+        cout << " --";
         position++;
       }
-      cout << " " << (*j).weight;
+      cout << "  " << (*j).weight;
       position++;
     }
     while (position < VerticesNum)
     {
-      cout << " -";
+      cout << " --";
       position++;
     }
     cout << endl;
   }
 }
 
-//==============================================================================
+
 // NodeInfo Definitions
-//==============================================================================
 struct strNodeInfo
 {
   int NodeNum;	// Node Number
@@ -246,11 +271,10 @@ bool compareShortPath(NodeInfo& N1, NodeInfo& N2)
   return false;
 }
 
-//==============================================================================
+
 // Class PriorityQueue
 // The value of the PriorityQueue is to always have access to the vertex with the 
 // next shortest link in the shortest path calculation at the top of the queue
-//==============================================================================
 class PriorityQueue {
   public:
     PriorityQueue();
@@ -263,19 +287,26 @@ class PriorityQueue {
     int size();
     
   private:
-    list<NodeInfo> pq;		// List of known nodes/paths ordered by minDist
+    list<NodeInfo> pq;
 };
+
+// Constructor of PriorityQueue Class
+// Creates an empty list of nodes
+PriorityQueue::PriorityQueue()
+{
+  pq.clear();
+}
 
 // changes the priority (node value) of queue element
 void PriorityQueue::chgPriority(NodeInfo n)
 {
   for(list<NodeInfo>::iterator i=pq.begin(); i!=pq.end(); ++i)
-    if ((*i) == n)
+    if ((*i).NodeNum == n.NodeNum)
     {
       (*i).ShortPath = n.ShortPath;
       (*i).through = n.through;
     }
-  pq.sort(compareMinDist);
+  pq.sort(compareShortPath);
 }
 
 // removes the top element of the queue 
@@ -319,8 +350,8 @@ NodeInfo PriorityQueue::top()
   if (! pq.empty())
   {
     list<NodeInfo>::iterator i=pq.begin();
-    n.nodeName = (*i).nodeName;
-    n.minDist = (*i).minDist;
+    n.NodeNum = (*i).NodeNum;
+    n.ShortPath = (*i).ShortPath;
     n.through = (*i).through;
   }
   return n;
@@ -332,20 +363,19 @@ int PriorityQueue::size()
   return pq.size();
 }
 
-//==============================================================================
+
 // ShortestPath Class
 // Implements the mechanics of Dijkstra’s algorithm
-//==============================================================================
 class ShortestPath
 {
   public:
     ShortestPath();
     ShortestPath(Graph g);
     list<int> path(int u, int w);
-    int path_size(char u, char w);
+    int path_size(int u, int w);
   
   private:
-    Graph graph;		// Graph used by Diajkstra's Algorithm
+    Graph graph;
 };
 
 // Construct ShortestPath Class
@@ -360,10 +390,10 @@ ShortestPath::ShortestPath(Graph g)
 }
 
 // find shortest path between u-w and returns the sequence of vertices representing shorest path u-v1-v2-…-vn-w.
-list<char> ShortestPath::path(int u, int w)
+list<int> ShortestPath::path(int u, int w)
 {
   // Initialization
-  list<int> candidates = graph.vertices()
+  list<int> candidates = graph.vertices();
   list<int> desiredPath;
   list<NodeInfo> minPaths;
   PriorityQueue p;
@@ -379,7 +409,7 @@ list<char> ShortestPath::path(int u, int w)
   while ((!candidates.empty()) && (lastSelected.NodeNum !=w))
   {
     // For each node in candidate list calculate the cost to reach that candidate through lastSelected 
-    for(list<char>::iterator i=candidates.begin(); i != candidates.end(); ++i)
+    for(list<int>::iterator i=candidates.begin(); i != candidates.end(); ++i)
     {
       n.NodeNum=*i;
       n.ShortPath=lastSelected.ShortPath + graph.get_edge_value(lastSelected.NodeNum,*i);
@@ -398,7 +428,7 @@ list<char> ShortestPath::path(int u, int w)
   // Go backward from 'w' to 'u' adding nodes in that path to desiredPath list
   lastSelected=minPaths.back();
   desiredPath.push_front(lastSelected.NodeNum);
-  while(lastSelected.nodeName!=u)
+  while(lastSelected.NodeNum!=u)
   {
     for(list<NodeInfo>::iterator i=minPaths.begin(); i != minPaths.end(); ++i)
       if ((*i).NodeNum==lastSelected.through)
@@ -410,4 +440,92 @@ list<char> ShortestPath::path(int u, int w)
   return desiredPath;
 }
 
+// return the path cost associated with the shortest path
+int ShortestPath::path_size(int u, int w)
+{
+  int PathCost=0;
+  int CurrentNode=0;
+  list<int> NodePath;
+  
+  // Calculate the shortest path from 'u' to 'w' and then sum up edge weights in this path
+  NodePath = path(u,w);
+  CurrentNode=NodePath.front();
+  NodePath.pop_front();
+  for(list<int>::iterator i=NodePath.begin(); i!=NodePath.end(); ++i)
+  {
+    PathCost = PathCost + graph.get_edge_value(CurrentNode,(*i));
+    CurrentNode = (*i);
+  }
+  return PathCost; 
+}
 
+// Overload operator << to print list<int> variables
+ostream &operator<<(ostream &output, list<int> L)
+{
+   for(list<int>::iterator i=L.begin(); i != L.end(); ++i)
+     output << *i << " ";
+   return output;
+}
+
+void execution (Graph g)
+{
+  double density = static_cast<double>(g.E())/((static_cast<double>(g.V())*static_cast<double>(g.V())-1)/2)*100;	// calculate real density
+// how many vertices
+  cout << "Vertices Numbers: " << g.V() << endl;
+
+// what are vertices
+  list<int> ver = g.vertices();
+  cout << "Vertices are: ";
+  for(list<int>::iterator i=ver.begin(); i != ver.end(); ++i)
+    cout << *i << " ";
+  cout << endl;
+
+// the real density
+  cout << "Edges Numbers: " << g.E() << " (density: " << density << "%)" << endl;
+
+// the whole graph
+  cout << "Graph: " << endl;
+  g.display();
+
+// Display the shortest path
+  int reachVert=0, TotalPath=0, AvgPath=0;
+  ShortestPath sp(g);
+  for (list<int>::iterator i=ver.begin(); i != ver.end(); ++i) 
+  {
+    int src = ver.front();
+    int dst = (*i);
+    list<int> p = sp.path(src,dst);
+    int ps = sp.path_size(src,dst);
+    if (ps != DEADBEEF)
+      cout << "ShortestPath (" << src << " to " << dst << "): " << ps << " -> " << p << endl;
+    else
+      cout << "ShortestPath (" << src << " to " << dst << "): " << "** UNREACHABLE **" << endl;      
+    if (ps!=DEADBEEF)
+    {
+      reachVert++;		// Sum up reached nodes 
+      TotalPath += ps;	// Sum up shortest paths found
+    }
+  }  
+
+  // Calculate average shortest path and print it out
+  if (reachVert!=0)
+    AvgPath = TotalPath / reachVert;	
+  else
+    AvgPath = 0;
+  cout << endl << "AVG ShortestPath Size (reachVert: " << reachVert << " - TotalPath: " << TotalPath << "): " << AvgPath << endl;
+
+}
+
+// main
+int main()
+{  
+  // 50 nodes, 20% density, 1 min path, 10 max path
+  Graph g1(50, 0.2, 1, 10);
+  execution(g1);
+
+  // 50 nodes, 40% density, 1 min path, 10 max path
+  Graph g2(50, 0.4, 1, 10);
+  execution(g2);
+  
+  return 0;  
+}
